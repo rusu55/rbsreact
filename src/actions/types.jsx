5c05895ed4c6876b580716e6ce57dfbf012c05d6
Bogdan <rusu55@yahoo.com>
2019-06-21 16:52:01 -0400

    updated

--- src/actions/types.jsx
+++ src/actions/types.jsx
@@ -1,5 +1,7 @@
 export const SET_ALERT = 'SET_ALERT'
 export const REMOVE_ALERT = 'REMOVE_ALERT'
+
+export const SIDEBAR_VISIBLE = 'SIDEBAR_VISIBLE'
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
 export const LOGIN_FAIL = 'LOGIN_FAIL'
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
